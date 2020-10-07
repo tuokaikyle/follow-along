6b6ea93cf24dedf33036eab3d90a9d71883f9b99
tuokaikyle <867367934@qq.com>
2020-10-07 14:16:05 +1000

    mongodb setup

--- todo-fullstack/todo-backend/index.js
+++ todo-fullstack/todo-backend/index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { MongoClient, ObjectID } = require("mongodb");
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
+let db;
+MongoClient.connect(
+  "mongodb://localhost:27017/todos",
+  { useUnifiedTopology: true },
+  async function (err, client) {
+    if (err) throw err;
+    db = client.db("todos");
+    console.log("mongodb connected");
+  }
+);
 
 app.get("/", (req, res) => {
   res.json("backend setup: browser tester good");
+});
+
+app.get("/todos", async (req, res) => {
+  const results = await db.collection("todos").find().toArray();
+  res.json(results);
 });
 
 app.listen(3001, () => {
